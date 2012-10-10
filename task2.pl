63b07f16770d8f929e3d2401828b6c9d19e0ad2c
Kirill Mokhov <krlmhv@mail.ru>
2012-10-11 01:04:03 +0600

    Update new version
    Обновление. Пока заметил, что не выложил первую версию, уже написал
    вторую.

--- task2.pl
+++ task2.pl
@@ -1,19 +1,35 @@
-$str="";
-open (OUTPUT, ">output.txt") or die $!;
+#!/usr/local/bin/perl
+#Hi. You need to write "perl task2.pl filename [filename]" to exec this. The result will be in output.txt"
+$str=""; #string
+$cv=""; #current value
+$cumv=""; #current used memory value
+open (OUTPUT, ">output.txt");
 $,=" " or die $!;
 while($file=shift)
 {
-	open FILE, $file or die $!;
-	@content = <FILE> or die $!;
-	foreach $line(@content)
+	open FILE, $file or die "Can't open file.";
+	if(($cv=-s ($file))<5242880)
 	{
-		$str .= $line or die $!;
+		if($cumv<524288000)
+		{
+			@content=<FILE> or die "Can't read, sorry.";
+			foreach $line(@content)
+			{
+				$str.=$line or die "Can't read, sorry.";
+			}
+			close FILE or die "Can't close file.";
+			$cumv=$cumv+$cv;
+			#print "121\n";
+		}
+		else {die "Out of memory!"};
 	}
-	close FILE or die $!;
+	else {die "It's a very big file here."};
+	#print $file."\n";
 }
-$str =~ s/\b[^\s]*[^0-9\s][^\s]*\b/ /g or die $!;
-$str =~ s/\s+/ /g or die $!;
-@_ = sort {$a <=> $b} split(' ', $str) or die $!;
-open (OUTPUT, ">>output.txt") or die $!;
+$str=~s/\b[^\s]*[^0-9\s][^\s]*\b/ /g or die "Empty";
+$str=~s/\s+/ /g or die "Empty";
+@_=sort {$a<=>$b} split(' ',$str) or die $!;
+open (OUTPUT, ">>output.txt");# or die $!;
 print OUTPUT @_ or die $!;
 close OUTPUT or die $!;
+print "Check out the output.txt file.";
