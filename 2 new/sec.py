b4cb430876ee3f560780aa542eea4a85b9a5688f
Kirill Mokhov <krlmhv@mail.ru>
2012-12-10 20:14:50 +0600

    Вторая задача (переделанная)
    Переделанная вторая задача, описание в help.txt

---
+++ 2 new/sec.py
@@ -0,0 +1,36 @@
+import re
+import sys
+import os
+
+def main():
+	if len(sys.argv) > 2:
+		t = ''
+		for i in range(1,len(sys.argv)-1):
+			if os.path.exists(sys.argv[i]):
+				try:
+					try:
+						f = open(sys.argv[i],'r')
+						t += f.read()
+						f.close()
+					except IOError as e:
+						print "This file can't be open!"
+					t = "\n" + t + "\n "
+					t = t.replace(" ",'\n')
+				except MemoryError as e:
+					print "Not enough memory!"
+			else:
+				print "Missing file!"
+				continue
+		regExp = re.compile('^\s*(\d+)\s*$',re.MULTILINE)
+		numbers = regExp.findall(t) 
+		numbers.sort(key=int)
+		f = open(sys.argv[-1],'w')
+		for i in range(len(numbers)):
+			f.write(numbers[i] + "\n")
+		f.close()
+		print "All done."	
+	else:
+		print "Input more files please!"
+		print "Example: sec.py file1 [file2] filen "
+		print "file1...filen-1 - data files, filen - output file"
+main()
