b7958522205085f1c18b56a2350bfb9b9bf70882
Kirill Mokhov <krlmhv@mail.ru>
2012-12-23 00:04:41 +0600

    2 new new
    Исправил замечания. Также файл 3.txt у себя на компьютере закрыл
    полностью, программа выдает ошибку при его чтении, как Вы и просили
    (сюда его не получится залить, но создать его думаю не проблема).

--- 2 new/sec.py
+++ 2 new/sec.py
@@ -5,30 +5,35 @@ import os
 def main():
 	if len(sys.argv) > 2:
 		t = ''
-		for i in range(1,len(sys.argv)-1):
-			if os.path.exists(sys.argv[i]):
-				try:
-					try:
-						f = open(sys.argv[i],'r')
+		try:
+			for i in range(1,len(sys.argv)-1):
+				if os.path.exists(sys.argv[i]):
+						try:
+							f = open(sys.argv[i],'r');
+						except IOError as e:
+							print "Can't open or read file",sys.argv[i]
+							continue
 						t += f.read()
 						f.close()
-					except IOError as e:
-						print "This file can't be open!"
-					t = "\n" + t + "\n "
-					t = t.replace(" ",'\n')
-				except MemoryError as e:
-					print "Not enough memory!"
-			else:
-				print "Missing file!"
-				continue
-		regExp = re.compile('^\s*(\d+)\s*$',re.MULTILINE)
-		numbers = regExp.findall(t) 
-		numbers.sort(key=int)
-		f = open(sys.argv[-1],'w')
-		for i in range(len(numbers)):
-			f.write(numbers[i] + "\n")
-		f.close()
-		print "All done."	
+						t = "\n" + t + "\n "
+						t = t.replace(" ",'\n')
+				else:
+					print "Missing file!"
+					continue
+			regExp = re.compile('^\s*(\d+)\s*$',re.MULTILINE)
+			numbers = regExp.findall(t) 
+			numbers.sort(key=int)
+			f = open('output.txt','w')
+			for i in range(len(numbers)):
+				f.write(numbers[i] + "\n")
+			f.close()
+		except Exception as e:
+			print "Something is wrong!"
+		except IOError as e:
+			print "This file can't be open!"
+		except MemoryError as e:
+			print "Not enough memory!"
+		print "All done."
 	else:
 		print "Input more files please!"
 		print "Example: sec.py file1 [file2] filen "
--- 2 new/sec2.py
+++ 2 new/sec2.py
@@ -5,29 +5,34 @@ import os
 def main():
 	if len(sys.argv) > 1:
 		t = ''
-		for i in range(1,len(sys.argv)):
-			if os.path.exists(sys.argv[i]):
-				try:
-					try:
-						f = open(sys.argv[i],'r')
+		try:
+			for i in range(1,len(sys.argv)):
+				if os.path.exists(sys.argv[i]):
+						try:
+							f = open(sys.argv[i],'r');
+						except IOError as e:
+							print "Can't open or read file",sys.argv[i]
+							continue
 						t += f.read()
 						f.close()
-					except IOError as e:
-						print "This file can't be open!"
-					t = "\n" + t + "\n "
-					t = t.replace(" ",'\n')
-				except MemoryError as e:
-					print "Not enough memory!"
-			else:
-				print "Missing file!"
-				continue
-		regExp = re.compile('^\s*(\d+)\s*$',re.MULTILINE)
-		numbers = regExp.findall(t) 
-		numbers.sort(key=int)
-		f = open('output.txt','w')
-		for i in range(len(numbers)):
-			f.write(numbers[i] + "\n")
-		f.close()
+						t = "\n" + t + "\n "
+						t = t.replace(" ",'\n')
+				else:
+					print "Missing file!"
+					continue
+			regExp = re.compile('^\s*(\d+)\s*$',re.MULTILINE)
+			numbers = regExp.findall(t) 
+			numbers.sort(key=int)
+			f = open('output.txt','w')
+			for i in range(len(numbers)):
+				f.write(numbers[i] + "\n")
+			f.close()
+		except Exception as e:
+			print "Something is wrong!"
+		except IOError as e:
+			print "This file can't be open!"
+		except MemoryError as e:
+			print "Not enough memory!"
 		print "All done."	
 	else:
 		print "Input more files please!"
