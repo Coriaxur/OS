9078c627ee048f693ee4de09b65dabfdb8f8af5f
Kirill Mokhov <krlmhv@mail.ru>
2013-01-25 22:40:11 +0600

    Пятая задача
    Обновление

--- 5/5.c
+++ 5/5.c
@@ -6,7 +6,6 @@
 
 #define NumberOfContexts 16
 #define StackSize 4096
-
 int MyInterval = 100;
 int ThreadTicks = 15;
 int CurrentContextId = 0;
@@ -28,6 +27,8 @@ struct itimerval AlarmInterval;
 
 ucontext_t SchedulerContext;
 
+void *signal_stack;
+
 void thread_exit()
 {
 	MyTreads[CurrentContextId].ContextStatus = 0;
@@ -36,9 +37,9 @@ void thread_exit()
 int thread_create(void(*fucnt)())
 {
 	int id;
-	for(id=1; id<NumberOfContexts; id++)
+	for(id = 1; id < NumberOfContexts; id++)
 	{
-		if(MyTreads[id].ContextStatus==0)
+		if(MyTreads[id].ContextStatus == 0)
 		{
 			MyTreads[id].ContextId = id;
 			MyTreads[id].ContextStatus = 1;
@@ -52,6 +53,8 @@ int thread_create(void(*fucnt)())
 	MyTreads[id].ExitContext.uc_stack.ss_size = sizeof(MyTreads[id].ExitStack);
 	MyTreads[id].ExitContext.uc_link = &SchedulerContext;
 	makecontext(&MyTreads[id].ExitContext, thread_exit, 0);
+
+
 	getcontext(&MyTreads[id].Context);
 	MyTreads[id].Context.uc_stack.ss_sp = MyTreads[id].Stack;
 	MyTreads[id].Context.uc_stack.ss_size = sizeof(MyTreads[id].Stack);
@@ -64,8 +67,8 @@ int thread_create(void(*fucnt)())
 void thread_sleep(int counts)
 {
 	MyTreads[CurrentContextId].ContextStatus = 2;
-	MyTreads[CurrentContextId].ContextSleepTime=counts;
-	int TempContextId=CurrentContextId;
+	MyTreads[CurrentContextId].ContextSleepTime = counts;
+	int TempContextId = CurrentContextId;
 	CurrentContextId = 0;
 	swapcontext(&MyTreads[TempContextId].Context, &SchedulerContext);
 }
@@ -90,10 +93,11 @@ void thread_2()
 	}
 }
 
+
 void thread_wait(int tid)
 {
-	int l=(MyInterval*1000)%1000000;
-	while (MyTreads[tid].ContextStatus!=0) 
+	int l = (MyInterval*1000)%1000000;
+	while (MyTreads[tid].ContextStatus != 0) 
 	{
 		poll(NULL,0,l);
 	}
@@ -101,25 +105,25 @@ void thread_wait(int tid)
 
 void swap_threads(int tid)
 {
-	if (tid!=0)
+	if (tid != 0)
 	{
 		CurrentContextId = tid;
 		swapcontext(&SchedulerContext, &MyTreads[CurrentContextId].Context);
 	}
 }
 
-void schedul_function(int signal)
+void schedul_function()
 {
 	int NextId = 0;
 	int CurrentId = 0;
-	while(CurrentId<NumberOfContexts)
+	while(CurrentId < NumberOfContexts)
 	{
-		if (MyTreads[CurrentId].ContextStatus==2)
+		if (MyTreads[CurrentId].ContextStatus == 2)
 		{//Sleeping?
 			MyTreads[CurrentId].ContextSleepTime -= 1;
-			if (MyTreads[CurrentId].ContextSleepTime==0)
+			if (MyTreads[CurrentId].ContextSleepTime == 0)
 			{
-				MyTreads[CurrentId].ContextStatus=1;//Active!
+				MyTreads[CurrentId].ContextStatus = 1;//Active!
 			}
 		}
 		CurrentId++;
@@ -127,9 +131,9 @@ void schedul_function(int signal)
 
 	//Moving...
 	CurrentId = 0;
-	while(CurrentId<NumberOfContexts)
+	while(CurrentId < NumberOfContexts)
 	{
-		if ((MyTreads[CurrentId].ContextStatus==1)&&(CurrentContextId!=CurrentId))
+		if ((CurrentContextId!=CurrentId)&&(MyTreads[CurrentId].ContextStatus==1))
 		{
 			NextId = CurrentId;
 			break;
@@ -139,8 +143,24 @@ void schedul_function(int signal)
 	swap_threads(NextId);
 }	
 
+void scheduler_init(ucontext_t * init_context)
+{
+	getcontext(init_context);
+	init_context->uc_stack.ss_sp=signal_stack;
+	init_context->uc_stack.ss_size=StackSize;
+	init_context->uc_stack.ss_flags=0;
+	sigemptyset(&init_context->uc_sigmask);
+	makecontext(init_context,schedul_function,0);
+}
+
 int main()
 {
+	signal_stack = malloc (StackSize);
+	if (signal_stack == NULL) 
+	{
+       	printf("Cant malloc\n");
+       	exit(1);
+    }
 	int i;
 	for (i=1; i<NumberOfContexts; i++)
 	{
@@ -159,7 +179,7 @@ int main()
 	};
 	
 	signal(SIGALRM, schedul_function); 
-	
+	scheduler_init(&SchedulerContext);
 	int Thread1 = thread_create(thread_1);
 	int Thread2 = thread_create(thread_2);
 	thread_wait(Thread1);
